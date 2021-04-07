9bc296003f15a18ec94e589613fb217b9c85f97b
Alex Gass <alexander.gass@mail.mcgill.ca>
2021-04-07 16:43:26 -0400

    Added comments outlining goal

--- foundation_getImages.py
+++ foundation_getImages.py
@@ -1,3 +1,8 @@
 #This code will ideally be used for the unsupervised training algorithm (in this case, K-means), which generates a bank of filters D
+#extract 16-by-16 pixel grayscale patches represented as a vector of 256 pixel intensities
+
+#I.E. Crop and scale images while keeping RGB channels 
+
+
 
 import numpy as np
