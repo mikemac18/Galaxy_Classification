0cc87573c32eee35fc6d07b89890ad271344b9ed
Alex Gass <alexander.gass@mail.mcgill.ca>
2021-04-07 20:16:40 -0400

    Implemented crop image

--- foundation_getImages.py
+++ foundation_getImages.py
@@ -18,24 +18,23 @@ imPath_test = 'images_training_rev1\\images_training_rev1\\'
 imPath_training = 'images_training_rev1\\images_training_rev1\\'
 pic_Index = '100008.jpg'
 
-#Attempt to test crop and scale on a single image
-
+#Attempt to scale a single image
 
 origImage = cv2.imread(basepath+imPath_training+pic_Index)
 
-print(origImage.shape)
 newx,newy = origImage.shape[1]/4,origImage.shape[0]/4 #new size (w,h)
 
-newimage = cv2.resize(origImage,(int(newx),int(newy)))
+scaled_image = cv2.resize(origImage,(int(newx),int(newy)))
 
-print(newimage.shape)
 
 #cv2.imshow("original image",origImage)
 #cv2.imshow("resize image",newimage)
 
+
 #Crop image from 424x424 to 160x160. To do we crop axes from {x=0, y=0, x=424, y=424 } => {x=132, y=132, x=292, 400=292}
 crop_img = origImage[132:292, 132:292] 
-cv2.imwrite
+
+cv2.imwrite("testCrop.jpg", crop_img)
 cv2.imshow("cropped", crop_img)
 
 cv2.waitKey(0)
