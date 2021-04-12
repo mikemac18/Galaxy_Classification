6b398ec0b7b0fa689dbf6122499541fc216281a8
SamMerc <samson.mercier18@gmail.com>
2021-04-12 13:01:03 -0400

    Added a function to get the 4x4 patches from each 16x16 image. Used the same method as in the crop function to get the dimensions for the 4 patches and crop the image around those dimensions. Need to test code to make sure it works and need to figure out if we want to extract each patch one at a time or all at once.

--- getImages_withScaling.py
+++ getImages_withScaling.py
@@ -20,11 +20,11 @@ class GetImage(object):
     #Load raw image files
     
     #initialize the path (TODO - make basepath an input parameter, don't hardcode too much)
-    def __init__(self, pic_Index= '100008.jpg'):
+    def __init__(self, pic_Index='100008.jpg'):
 
-        self.basepath = 'C:\\Users\\Admin\\321_galaxies\\'
-        self.imPath_test = 'images_training_rev1\\images_training_rev1\\'
-        self.imPath_training = 'images_training_rev1\\images_training_rev1\\'
+        self.basepath = '/Users/samsonmercier/Desktop/PHYS321_FinalProj/Image_class/'
+        self.imPath_test = 'Raw/images_test_rev1/'
+        self.imPath_training = 'Raw/images_training_rev1/'
         self.pic_Index = pic_Index
 
         #create variable that stores the image
@@ -46,10 +46,33 @@ class GetImage(object):
         dimensions = (int(new_size), int(new_size))
         self.image = cv2.resize(self.image, dimensions)
         return self
+    
+    #function that crops out 4x4 patches of our image. Each image will then have 4 patches.
+    def patch(self, p_size=4, patch_num):
+        patch_number = 'patch'+str(patch_num)
+        center1 = 2
+        center2 = 6
+        dim = int(p_size/2)
+        patchmin1 = center1 - dim
+        patchmax1 = center1 + dim
+        patchmin2 = center2 - dim
+        patchmax2 = center2 + dim
+        patch1 = self.image[patchmin1:patchmax1, patchmin1:patchmax1]
+        patch2 = self.image[patchmin1:patchmax1, patchmin2:patchmax2]
+        patch3 = self.image[patchmin2:patchmax2, patchmin1:patchmax1]
+        patch4 = self.image[patchmin2:patchmax2, patchmin2:patchmax2]
+        if patch_number == 'patch1': 
+            self.image = patch1
+        if patch_number == 'patch2': 
+            self.image = patch2
+        if patch_number == 'patch3': 
+            self.image = patch3
+        if patch_number == 'patch4': 
+            self.image = patch4
+        return self.image
 
 
-"""
-Testing the class by creating a 'galaxyPic' object, cropping it, scaling it, and saving the new image
+#Testing the class by creating a 'galaxyPic' object, cropping it, scaling it, and saving the new image
 
 galaxyPic = GetImage()
 galaxyPic.crop()
@@ -57,4 +80,3 @@ galaxyPic.scale()
 
 cv2.imwrite("testCrop-and-Scale.jpg", galaxyPic.image)
 cv2.waitKey(0)
-"""
